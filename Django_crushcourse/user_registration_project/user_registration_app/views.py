80bf8b3795d4f59b6f24d3e5684ca05e8147f7d2
andmzz96 <94182411+AndreaMazzoleniOOI@users.noreply.github.com>
2024-01-11 11:14:44 +0100

    Lecture 5
    Create a user login funcctionality

--- Django_crushcourse/user_registration_project/user_registration_app/views.py
+++ Django_crushcourse/user_registration_project/user_registration_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import Feature
 # Create your views here.
 
@@ -16,3 +18,36 @@ def counter(request):
         'length': len(words.split())
     }
     return render(request, 'counter.html', context)
+
+
+def register(request):
+    if request.method != 'POST':
+        return render(request, 'register.html')
+    print(request.POST)
+
+    for field in request.POST:
+        print(field)
+        if not request.POST[field]:
+            print(field)
+            messages.info(request, 'Please fill up all the fields!')
+            return redirect('register')
+
+    user = request.POST['username']
+    email = request.POST['email']
+    psw = request.POST['password']
+    psw2 = request.POST['password2']
+
+    if User.objects.filter(username=user).exists():
+        messages.info(request, 'Username is already in use')
+        return redirect('register')
+    if psw != psw2:
+        messages.info(request, 'Password does not match')
+        return redirect('register')
+    if User.objects.filter(email=email).exists():
+        messages.info(request, 'Email is already in use')
+        return redirect('register')
+
+    # Create user in db (USER field)
+    user = User.objects.create_user(username=user, email=email, password=psw)
+    user.save()
+    return redirect('login')
