a55a0a0ab1a75556c73036e73493b881c52c88d5
andmzz96 <94182411+AndreaMazzoleniOOI@users.noreply.github.com>
2024-01-15 15:43:40 +0100

    Example Blog Project
    Part 2

--- Django_crushcourse/myblog_project/posts/views.py
+++ Django_crushcourse/myblog_project/posts/views.py
@@ -6,3 +6,8 @@ def index(request):
     posts = Post.objects.all()
     return render(request, 'index.html', {'posts': posts})
 
+def post(request, pk):
+    post = Post.objects.get(id=pk)
+    return render(request, 'post.html', {'post': post})
+
+
