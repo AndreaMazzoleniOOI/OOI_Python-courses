8649c29bf5b02563db46caf0d13ecdac4d0d9644
andmzz96 <94182411+AndreaMazzoleniOOI@users.noreply.github.com>
2024-01-15 12:12:08 +0100

    Update settings.py
    Connect to your postrgres db

--- Django_crushcourse/blog_project/blog_project/settings.py
+++ Django_crushcourse/blog_project/blog_project/settings.py
@@ -76,9 +76,16 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER': 'postgres',
+        'PASSWORD': 'silvestro',
+        'HOST': 'localhost'
     }
 }
 
